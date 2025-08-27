d6c65cb5c8caa9f5727023e17aa301314516c2a2
nealandin <nealandin@me.com>
2025-08-27 16:01:29 +0200

    roles implemented

--- public/js/client.js
+++ public/js/client.js
@@ -1,18 +1,31 @@
 const socket = io();
 
 // html elements
-const testText = document.querySelector('#test-text');
+const titleText = document.querySelector('.title-text');
+const instructionText = document.querySelector('#instruction-text');
 const joinRoomButton = document.getElementById('join-room');
-
+const roleButtons = document.getElementsByClassName("role-button");
 
 // socket events sent from server
 socket.on('next-instruction', (data) => { // Listens for the 'next-instruction' event from the server
-    testText.innerHTML = data; // Updates the text content of the testText element
+    titleText.style.display = "none";
+    instructionText.innerHTML = data; // Updates the text content of the testText element
 });
 
-// page event listeners
-joinRoomButton.addEventListener('click', () => { // Adds a click event listener to the joinRoomButton element
-    socket.emit('join-room', 'room1'); // Sends a 'join-room' event to the server
-    joinRoomButton.disabled = true; // Disables the joinRoomButton 
-    joinRoomButton.style.display = 'none'; // Hides the joinRoomButton 
+socket.on('time-for-next-instruction', () => {
+    socket.emit("getInstruction");
 });
+
+for(let i = 0; i < roleButtons.length; i++){
+    const button = roleButtons[i];
+    button.addEventListener('click', () => { 
+        const role = button.id;
+        socket.emit('role-chosen', role); 
+        for(let i = 0; i < roleButtons.length; i++){
+            roleButtons[i].style.display = 'none';
+        }
+        titleText.innerHTML = "WAIT FOR INSTRUCTIONS";
+    });
+}
+
+
--- src/server.js
+++ src/server.js
@@ -32,33 +32,6 @@ const io = new Server(server, {
 });
 
 
-
-
-
-// FUNCTIONS
-
-  // creating and adding user object to users array
-  const createUser = (socketId) => {
-    users[socketId] = {
-      id: socketId,
-      name: "",
-      room: "",
-      talking: false,
-    }
-    console.log("user array with user added: " , users);
-  }
-
-  // removing user object from users array
-  const removeUser = (socketId) => {
-    delete users[socketId];
-    console.log("user array with user removed: " , users);
-  }
-
-  const getRandomFromArray = (array) => {
-    return array[Math.floor(Math.random() * array.length)];
-  }
-
-
 // DATA
 
   const users = [];
@@ -86,17 +59,67 @@ const io = new Server(server, {
     "Wave your hands",
   ]
 
+  const role1 = ["role1-walk", "role1-talk"];
+  const role2 = ["role2-walk", "role2-talk"];
+  const role3 = ["role3-walk", "role3-talk"];
+
+
+
+
+// FUNCTIONS
+
+  // creating and adding user object to users array
+  function createUser(socketId) {
+    
+    users[socketId] = {
+      id: socketId,
+      name: "",
+      room: "",
+      talking: false,
+      role: "",
+      roleActions: [],
+      currentInstruction: "",
+    }
+
+    console.log("user array with user added: " , users);
+  }
+
+  function assignRole(socketId, role) {
+    if (role === "role1") {
+      users[socketId].role = role;
+      users[socketId].roleActions = role1;
+    } else if (role === "role2") {
+      users[socketId].role = role;
+      users[socketId].roleActions = role2;
+    } else if (role === "role3") {
+      users[socketId].role = role;
+      users[socketId].roleActions = role3;
+    }
+    console.log("user role assigned: " + users[socketId].role);
+  }
+
+  // removing user object from users array
+  function removeUser(socketId) {
+    delete users[socketId];
+    console.log("user array with user removed: " , users);
+  }
+
+  function getRandomFromArray(array) {
+    console.log("array: " + array);
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+
+
 
 
 // SOCKET.IO 
 
   io.on('connection', (socket) => { // When a client connects to the server
-      console.log('a user connected'); // Logs a message to the console
-      
-      socket.emit('update-text', 'Psst! </br> (sent from server)'); // Sends a message to the client
+      console.log('a user connected:' + socket.id); // Logs a message to the console
       
       socket.on('disconnect', () => { // When a client disconnects from the server
-          console.log('user disconnected'); // Logs a message to the console
+          console.log('user disconnected: ' + socket.id); // Logs a message to the console
 
           rooms.forEach((room) => { // Iterates through the rooms array
 
@@ -117,15 +140,22 @@ const io = new Server(server, {
 
       });
 
+      socket.on('role-chosen', (data) => {
+        console.log("role chosen: " + data);
+        createUser(socket.id);
+        assignRole(socket.id, data);
+      });
+
       socket.on('join-room', () => { // When a client wants to join a room
         console.log("user wanting to join: " + socket.id);
 
-        const stringToSend = socket.id + " joined room";
-        io.emit("messageToScreen", stringToSend);
 
-        createUser(socket.id);
+        //createUser(socket.id);
 
-        for(let room of rooms){ // Iterates through the rooms array
+        //const stringToSend = socket.id + " joined room";
+        //io.emit("messageToScreen", stringToSend);
+
+       /* for(let room of rooms){ // Iterates through the rooms array
 
           if(room.users.includes(users[socket.id])){ // Checks if the user is already in a room
             console.log("user already in room: " + room.name);
@@ -133,7 +163,7 @@ const io = new Server(server, {
             return; // Stops the function here if the user is already in a room (Does not continue down to place the user in a room)
           }
 
-        }
+        } 
 
         console.log("user not in room");
 
@@ -153,13 +183,26 @@ const io = new Server(server, {
           console.log(room);
 
         }
+        */
       });
 
       socket.on('next-GM-instruction', () => {
-        console.log("got next instruction");
-        const instructionToSend = getRandomFromArray(instructions);
-        console.log("instruction sent: " + instructionToSend);
-        io.emit("next-instruction", instructionToSend);
+        io.emit("time-for-next-instruction");
+      });
+
+      socket.on("getInstruction", () => {
+
+        let roleActions = users[socket.id].roleActions; // get the roleActions array from the user object
+        let instruction = getRandomFromArray(roleActions); // get a random instruction from the roleActions array
+        
+        if(users[socket.id].currentInstruction === instruction){ // if the instruction is the same as the last one
+          roleActions = roleActions.filter((action) => action !== instruction); // remove the instruction from the roleActions array
+          instruction = getRandomFromArray(roleActions); // get a new random instruction
+        } 
+ 
+        io.emit("next-instruction", instruction);
+        users[socket.id].currentInstruction = instruction; // set instruction to currentInstruction in user object
+        
       });
       
       
